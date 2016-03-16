f3dac28a4c03b3028ca4247a39d9047176b349f0
Camille Harang <mammique@yooook.net>
2016-03-16 14:58:33 +0100

    Parsing improvements for MarkDown to HTML.

--- README_to_html.py
+++ README_to_html.py
@@ -1,14 +1,64 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
-import markdown2
+import markdown2, commands
+from slugify import slugify
 
-header = open('header.html').read().decode('utf-8')
-readme = open('README.md').read().decode('utf-8')
-footer = open('footer.html').read().decode('utf-8')
+header  = open('header.html').read().decode('utf-8')
+readme  = open('README.md').read().decode('utf-8').split('\n')
+title   = u'\n'.join(readme[0:2])
+content = u'\n'.join(readme[3:])
+footer  = open('footer.html').read().decode('utf-8')
 
-render = u'\n'.join((header,
-                     markdown2.markdown(readme, extras=['fenced-code-blocks']),
-                     footer))
+title_html   = markdown2.markdown(title)
+content_html = markdown2.markdown(content, extras=['fenced-code-blocks'])
+content_html_lines = content_html.split('\n')
+
+version_count = commands.getstatusoutput('git log README.md | grep Author: | wc -l')[1]
+version_uuid  = commands.getstatusoutput('git log README.md | cut -d " " -f2 | head -c 7')[1]
+version       = u'pre-rfc_rev%s-%s' % (version_count, version_uuid)
+
+navs = [u'<li><a href="#menu_top">Checkoin</a></li>']
+
+for i in range(0, len(content_html_lines)):
+    line = content_html_lines[i]
+    if line.startswith('<h2>') and line.endswith('</h2>'):
+        text = line[4:-5]
+        slug = slugify(text)
+        content_html_lines[i] = u'<h2 id="%s"><a name="%s"></a>%s</h2>' % (slug, slug, text)
+        navs.append(u'<li><a href="#%s">%s</a></li>' % (slug, text))
+
+content_html = u'\n'.join(content_html_lines)
+
+body = u"""
+<header>%s</header>
+
+<div class="container">
+    <div class="row">
+
+        <div class="col-md-3 hidden-sm">
+            <div id="float_column">
+                <div id="menu" role="navigation">
+                    <ul class="nav nav-stacked" role="tablist">
+                        %s
+                    </ul>
+                </div>
+                <div id="doc_info">
+                    <a href="http://creativecommons.org/licenses/by-nd/4.0/"><img src="medias/cc-by-nd_88x31.png" /></a>
+                    <br />
+                    <span><label>Version:</label> %s</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-9 col-sm-12">
+        %s
+        </div>
+
+    </div>
+</div>
+""" % (title_html, u'\n                    '.join(navs), version, content_html)
+
+render = u'\n'.join((header, body, footer))
 
 open('index.html', 'w').write(render.encode('utf-8'))
